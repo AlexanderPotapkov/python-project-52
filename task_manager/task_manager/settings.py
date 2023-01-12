8d6dfa92bb94d976788a48ead880e92a33dc22bf
alexander_potapkov <xanben@mail.ru>
2023-01-12 15:14:41 +0300

    fix DATABASES

--- task_manager/task_manager/settings.py
+++ task_manager/task_manager/settings.py
@@ -87,7 +87,8 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600)
+my_db = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(my_db)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
