1993d9358dcd886d8352fc260de8544485c76434
alexander_potapkov <xanben@mail.ru>
2023-01-17 18:16:26 +0300

    fix some problems

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -23,6 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
