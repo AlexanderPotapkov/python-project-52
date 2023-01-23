bcf3ff29e675d2cbec6b46245382ebd95b524076
alexander_potapkov <xanben@mail.ru>
2023-01-23 17:15:33 +0300

    add template authentication.html

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,22 @@
+from django.utils.translation import gettext as _
 from django.views.generic import TemplateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 
 
 class IndexView(TemplateView):
     template_name = 'task_manager/index.html'
+
+
+class LoginUser(SuccessMessageMixin, LoginView):
+    form_class = AuthenticationForm
+    template_name = 'task_manager/authentication.html'
+    success_message = _("You were logged in")
+
+
+class LogoutUser(SuccessMessageMixin, LogoutView):
+    def dispatch(self, request, *args, **kwargs):
+        messages.success(request, _("You were logged out"))
+        return super().dispatch(request, *args, **kwargs)
