7018f0b082d14f063503cf0cd40a25f7e6182e4a
alexander_potapkov <xanben@mail.ru>
2023-01-23 14:46:46 +0300

    fix language

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -139,7 +139,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'ru-ru'
 
 LANGUAGES = [
     ('ru', _('Russian')),
