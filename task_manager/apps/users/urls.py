ed61290f9b76d200c2d3ff37262b8001a2192ff8
alexander_potapkov <xanben@mail.ru>
2023-01-31 16:24:05 +0300

    add update user

--- task_manager/apps/users/urls.py
+++ task_manager/apps/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import UsersView, RegisterUser
+from .views import UsersView, RegisterUser, UpdateUser
 
 urlpatterns = [
     path('', UsersView.as_view(), name='users'),
     path('create/', RegisterUser.as_view(), name='register'),
-    # path('<int:pk>/update/', UpdateUser, name='update'),
+    path('<int:pk>/update/', UpdateUser.as_view(), name='update'),
     # path('<int:pk>/delete/', #####, name='delete'),
 ]
--- task_manager/apps/users/views.py
+++ task_manager/apps/users/views.py
@@ -1,7 +1,12 @@
 from django.views.generic import ListView
 from django.urls import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
+from django.utils.translation import gettext as _
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+from django.contrib import messages
 
 from task_manager.apps.users.forms import RegisterUserForm
 from .models import User
@@ -14,9 +19,40 @@ class UsersView(ListView):
 
 
 class RegisterUser(SuccessMessageMixin, CreateView):
+    model = User
     form_class = RegisterUserForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('login')
+    extra_context = {'header': _('Registration'),
+                     'button': _('Register')}
 
 
-# class UpdateUser():
+class UpdateUser(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = User
+    form_class = RegisterUserForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users')
+    extra_context = {'header': _('Update user'),
+                     'button': _('Update')}
+
+    def test_func(self):
+        user = self.get_object()
+        return self.request.user.id == user.id
+
+    def form_valid(self, form):
+        form.save()
+        username = self.request.POST['username']
+        password = self.request.POST['password1']
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return redirect(self.success_url)
+
+    def handle_no_permission(self):
+        if self.request.user.is_authenticated:
+            message = _('You do not have rights to change another user.')
+            url = reverse_lazy('users')
+        else:
+            message = _('You are not authorized! Please sign in.')
+            url = reverse_lazy('login')
+        messages.warning(self.request, message)
+        return redirect(url)
