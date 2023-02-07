3c5580c661d7f041644d00c6db4039106577761d
alexander_potapkov <xanben@mail.ru>
2023-02-07 14:37:27 +0300

    add model Task

--- task_manager/apps/statuses/models.py
+++ task_manager/apps/statuses/models.py
@@ -5,12 +5,7 @@ from django.utils.translation import gettext as _
 class Status(models.Model):
     name = models.CharField(max_length=100, db_index=True,
                             verbose_name=_('Name'))
-    date_created = models.DateTimeField(auto_now_add=True)
+    date_create = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
-
-    class Meta:
-        verbose_name = _('Name')
-        verbose_name_plural = _('Names')
-        ordering = ['date_created', ]
--- task_manager/apps/tasks/models.py
+++ task_manager/apps/tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+from ..users.models import User
+from ..statuses.models import Status
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=100, db_index=True,
+                            verbose_name=_('Name'))
+    description = models.TextField(null=True, verbose_name=_('Description'))
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, verbose_name=_('Status'))
+    author = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name=_('Author'))
+    executor = models.ForeignKey(User, null=True, blank=True, on_delete=models.PROTECT, verbose_name=_('Executor'),
+                                 related_name='executor')
+    date_create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
