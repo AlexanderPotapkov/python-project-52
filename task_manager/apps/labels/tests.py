386ef4753cc7be7b02bf515310de549f0c2d9a0b
alexander_potapkov <xanben@mail.ru>
2023-02-15 14:55:43 +0300

    add tests for labels urls

--- task_manager/apps/labels/tests.py
+++ task_manager/apps/labels/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from .views import LabelsView, CreateLabel, UpdateLabel, DeleteLabel
+
+
+class TestLabelsUrls(SimpleTestCase):
+
+    def test_labels_view(self):
+        url = reverse('labels')
+        self.assertEquals(resolve(url).func.view_class, LabelsView)
+
+    def test_label_create(self):
+        url = reverse('create_label')
+        self.assertEquals(resolve(url).func.view_class, CreateLabel)
+
+    def test_label_update(self):
+        url = reverse('update_label', args='1')
+        self.assertEquals(resolve(url).func.view_class, UpdateLabel)
+
+    def test_label_delete(self):
+        url = reverse('delete_label', args='1')
+        self.assertEquals(resolve(url).func.view_class, DeleteLabel)
